4e5ef453698f5f20066997b9accb4cb195037b25
Chun Wei Chooi <chunwei2978@gmail.com>
2021-08-13 12:21:21 +0800

    Upload entire NetBeans Project to the Github.

---
+++ src/JPLConnection/JPLConnection.java
@@ -0,0 +1,45 @@
+package JPLConnection;
+
+import java.util.LinkedHashMap;
+import org.jpl7.Query;
+import org.jpl7.Term;
+
+public class JPLConnection {
+    //prolog file name
+    private final String PROLOG_FILE = "RulesForQuickSort.pl";
+    
+    //Check the connection of java to prolog file
+    public void consult() throws Exception{
+        //construct the connection query
+        String queryText = "consult('" + PROLOG_FILE + "')";
+        Query consultQuery = new Query(queryText);
+
+        //Execute the query and throw exception if failed
+        if(!consultQuery.hasSolution()){
+            throw new Exception("The consult process is failed.");
+        }
+        
+        consultQuery.close();
+    }
+
+    //Execute the query and return result
+    public Term sortingQuery(String queryText) throws Exception{
+        //Execute the query and return the result
+        return new Query(queryText).allSolutions()[0].get("SortedResult");
+    }
+    
+    //Convert result returned from Prolog to map
+    public LinkedHashMap<String, Term> convertSortingResultToMap
+        (Term term, LinkedHashMap<String,Term> sortedResultMap)throws Exception{
+        //Firstly, put a new data into the map
+        sortedResultMap.put(term.arg(1).arg(1).toString().replace("'", ""), term.arg(1).arg(2));
+
+        //Secondary, check the condition
+        //if fulfilling, then recursive call
+        //Else, return the final result
+        return((term.arg(2).arity()!=0)
+                ?convertSortingResultToMap(term.arg(2),sortedResultMap)
+                :sortedResultMap);
+    }
+}
+
